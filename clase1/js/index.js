44daaabc2e42ad814e04bd521cab2a3bc26e43fa
Grichu <Grichu>
2017-08-23 19:10:36 -0300

    Clase dos

---
+++ clase1/js/index.js
@@ -0,0 +1,17 @@
+//agrego contenido al texto introducido por el usuario
+alert("hola " + prompt())
+
+
+//sumo el texto introducido a una variable
+var numerovariable = 5
+alert( Number(prompt()) + numerovariable)
+
+
+//resto el texto introducido a una variable
+var numerovariable2 = 1
+alert( Number(prompt()) - numerovariable2)
+
+
+//sumo dos valores introducidos por usuario
+alert( Number(prompt()) + Number(prompt()))
+
